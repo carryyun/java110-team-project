4e24b8855199d7bd7e77ad183be8a531932bcd10
Kei <kei@192.168.0.111>
2018-10-05 15:42:09 +0900

    하지맛

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -22,6 +22,7 @@ public class App {
     }
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+       System.out.println("아 모야 나 되는거 맞음?");
         
     }
 }
