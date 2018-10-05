ce6fb0c4d72f6b249482437e99716399f6753ace
Song <tjgus2932@gmail.com>
2018-10-05 15:28:15 +0900

    wow

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,9 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+        wow();
+    }
+    public static void wow() {
+        System.out.println("WOW");
     }
 }
