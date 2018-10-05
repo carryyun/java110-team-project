5aa8d0350c443de0926d60f9a23b81fc4b9b0e31
kim <chfaos120@naver.com>
2018-10-05 15:10:06 +0900

    asdf

--- src/main/java/testlol.java
+++ src/main/java/testlol.java
@@ -3,6 +3,6 @@ public class testlol {
     //ㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅇㅇㅇㅇㅇㅇㅇㅇㅇ
     public static void main(String[] args) {
         System.out.println("안녕하세요 고정리입니다.");
-        //sf/fsda/a/as//ff
+        //sf/fsda/a/as//ffasdfsd
     }
 }
