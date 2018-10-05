04d727e0e623ca9dd7910d5cab49444596c1aa8b
Yun Gun Hee <bear930920@gmail.com>
2018-10-05 15:33:06 +0900

    .

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,16 +7,19 @@ public class App {
         return "Hello world.";
     }
     public String 집가자() {
-        
+        System.out.println("오늘 집에 못감");
         return "gogo";
     }
     public String 가자() {
+        System.out.println("오늘 집에 못감");
         return "gogo";
     }
     public String 자() {
+        System.out.println("오늘 집에 못감");
         return "gogo";
     }
     public String 고등어() {
+        System.out.println("오늘 집에 못감");
         return "go등어";
     }
     public static void main(String[] args) {
