c39f9db4969e63c62facc5dfdf1358cf92b2c5fb
Kei <kei@192.168.0.111>
2018-10-05 16:31:47 +0900

    다 삭제

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,31 +5,10 @@ public class App {
         return "Hello world.";
     }
      
-    public int add(int a, int b) {
-        return a+b;
-    }
-    public String 집가자() {
-        System.out.println("오늘 집에 못감");
-        return "gogo";
-    }
-    public String 가자() {
-        System.out.println("오늘 집에 못감");
-        return "gogㅇㅇㅇㅇㅇㅇㅇㅇㅇo";
-    }
-    public String 자() {
-        System.out.println("오늘 집에 못감");
-        return "gogo";
-    }
-    public String 고등어() {
-        System.out.println("오늘 집에 못감");
-        return "go등어";
-    }
-    public void 불금(String a , String b) {
-        
-    }
+   
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
-        System.out.println(new App().add(5,7));
+        System.out.println(new App());
         System.out.println("나도좀치자!!!!!!!!!!!!! !!!!!!!!!!!!");
         wow();
         String s1 = "인생은 ";
@@ -48,7 +27,7 @@ public class App {
             System.out.println(i);
             i++;
             App app = new App();
-            app.자();
+            
         }
         System.out.println("수정");
 
