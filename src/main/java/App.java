988fd917ed04e13a6a4c62cbbddde5845012a0bb
Yun Gun Hee <bear930920@gmail.com>
2018-10-05 15:40:12 +0900

    TEST

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,10 +10,6 @@ public class App {
         System.out.println("오늘 집에 못감");
         return "gogo";
     }
-<<<<<<< HEAD
- 
-    
-=======
     public String 가자() {
         System.out.println("오늘 집에 못감");
         return "gogo";
@@ -22,7 +18,6 @@ public class App {
         System.out.println("오늘 집에 못감");
         return "gogo";
     }
->>>>>>> branch 'master' of https://github.com/carryyun/java110-team-project.git
     public String 고등어() {
         System.out.println("오늘 집에 못감");
         return "go등어";
@@ -30,24 +25,12 @@ public class App {
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
         int i=1;
-<<<<<<< HEAD
-<<<<<<< HEAD
-        while(i<=100000) {
-            System.out.println(i);
-            i++;
-           
-            
-        }
-=======
-=======
         while(i<=100000) {
             System.out.println(i);
             i++;
             App app = new App();
             app.자();
         }
->>>>>>> branch 'master' of https://github.com/carryyun/java110-team-project.git
         System.out.println("수정");
->>>>>>> branch 'master' of https://github.com/carryyun/java110-team-project.git
     }
 }
