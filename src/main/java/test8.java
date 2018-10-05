e2733ef2b0ef6223d0849613afb5b219b7856ffe
kim <chfaos120@naver.com>
2018-10-05 14:58:46 +0900

    테투텥

--- src/main/java/test8.java
+++ src/main/java/test8.java
@@ -2,6 +2,6 @@
 public class test8 {
     //ㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅇㅇㅇㅇㅇㅇㅇㅇㅇ
     public static void main(String[] args) {
-        System.out.println("둠 칫 두둠칫 두묻미칫");
+        System.out.println("안녕하세요 고정리입니다.");
     }
 }
