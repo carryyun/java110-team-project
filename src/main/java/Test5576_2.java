ebe97ca851de5384c12ee425f2f6139802ac0985
Song <tjgus2932@gmail.com>
2018-10-05 16:31:34 +0900

    asdasdasd

--- src/main/java/Test5576_2.java
+++ src/main/java/Test5576_2.java
@@ -2,6 +2,7 @@
 public class Test5576_2 {
     public static void main(String[] args) {
         System.out.println("wwwwwwwwwwwwwwwwwwwwwwwwwwwww");
+        System.out.println("ASdasdasd");
     }
 
 }
