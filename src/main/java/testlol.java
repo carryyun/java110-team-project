92b669ee193b62d53bbc9510257e3ed3921f07ba
Song <tjgus2932@gmail.com>
2018-10-05 15:47:21 +0900

    sdfad

--- src/main/java/testlol.java
+++ src/main/java/testlol.java
@@ -1,12 +1,7 @@
 
 public class testlol {
-    //?…Ž?…Ž?…Ž?…Ž?…Ž?…Ž?…Ž?…Ž?…Ž?…Ž?…Ž?…‡?…‡?…‡?…‡?…‡?…‡?…‡?…‡?…‡
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("?•ˆ?…•?•˜?„¸?š” ê³ ì •ë¦¬ìž…?‹ˆ?‹¤.");
-=======
+        System.out.println("?ï¿½ï¿½?ï¿½ï¿½?ï¿½ï¿½?ï¿½ï¿½?ï¿½ï¿½ ê³ ì •ë¦¬ìž…?ï¿½ï¿½?ï¿½ï¿½.");
         System.out.println("ì•ˆë…•í•˜ì„¸ìš” ê³ ì •ë¦¬ìž…ë‹ˆë‹¤.");
-        //sf/fsda/a/as//
->>>>>>> branch 'master' of https://github.com/carryyun/java110-team-project.git
     }
 }
