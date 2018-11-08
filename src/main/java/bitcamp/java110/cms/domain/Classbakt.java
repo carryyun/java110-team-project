2094a834614c4544ac40a98062090162dcb96646
yeongroke <chfaos120@naver.com>
2018-11-08 20:17:19 +0900

    .

---
+++ src/main/java/bitcamp/java110/cms/domain/Classbakt.java
@@ -0,0 +1,26 @@
+package bitcamp.java110.cms.domain;
+
+public class Classbakt {
+  private int cbno;
+  private int meno;
+  private int ctno;
+  
+  public int getCbno() {
+    return cbno;
+  }
+  public void setCbno(int cbno) {
+    this.cbno = cbno;
+  }
+  public int getMeno() {
+    return meno;
+  }
+  public void setMeno(int meno) {
+    this.meno = meno;
+  }
+  public int getCtno() {
+    return ctno;
+  }
+  public void setCtno(int ctno) {
+    this.ctno = ctno;
+  }
+}
