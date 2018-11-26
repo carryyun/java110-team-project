3d12a54a142024396b509ada817c07dacd1de012
Kei <kei@KEIui-MacBook-Pro.local>
2018-11-26 09:43:27 +0900

    .

---
+++ src/main/java/bitcamp/java110/cms/domain/DashBoard.java
@@ -0,0 +1,13 @@
+package bitcamp.java110.cms.domain;
+
+public class DashBoard {
+  
+  private Mentee mentee;
+  private Mentor mentor;
+  private Classes classes;
+  private MiddleTag middletag;
+  private Product product;
+  
+  
+
+}
