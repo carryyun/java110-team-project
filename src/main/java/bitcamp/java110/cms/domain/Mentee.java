ff46cd1e8ebbf861db72be0c708098cce1ae8faa
Yun Gun Hee <bear930920@gmail.com>
2018-12-02 19:04:34 +0900

    .

--- src/main/java/bitcamp/java110/cms/domain/Mentee.java
+++ src/main/java/bitcamp/java110/cms/domain/Mentee.java
@@ -17,7 +17,7 @@ public class Mentee {
   protected String phot;
   protected char sell;
   protected String bkname;
-  protected int bkno;
+  protected String bkno;
   protected Date rgdt;
   protected char stat;
   protected String note;
@@ -26,8 +26,132 @@ public class Mentee {
   public Classes classes;
   public ClassOrder classorder;
   public Timetable timetable;
-  
-  
+  public int getNo() {
+    return no;
+  }
+  public void setNo(int no) {
+    this.no = no;
+  }
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public String getEmail() {
+    return email;
+  }
+  public void setEmail(String email) {
+    this.email = email;
+  }
+  public String getPwd() {
+    return pwd;
+  }
+  public void setPwd(String pwd) {
+    this.pwd = pwd;
+  }
+  public String getNick() {
+    return nick;
+  }
+  public void setNick(String nick) {
+    this.nick = nick;
+  }
+  public String getPhone() {
+    return phone;
+  }
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
+  public String getPstno() {
+    return pstno;
+  }
+  public void setPstno(String pstno) {
+    this.pstno = pstno;
+  }
+  public String getBas_addr() {
+    return bas_addr;
+  }
+  public void setBas_addr(String bas_addr) {
+    this.bas_addr = bas_addr;
+  }
+  public String getDet_addr() {
+    return det_addr;
+  }
+  public void setDet_addr(String det_addr) {
+    this.det_addr = det_addr;
+  }
+  public String getPhot() {
+    return phot;
+  }
+  public void setPhot(String phot) {
+    this.phot = phot;
+  }
+  public char getSell() {
+    return sell;
+  }
+  public void setSell(char sell) {
+    this.sell = sell;
+  }
+  public String getBkname() {
+    return bkname;
+  }
+  public void setBkname(String bkname) {
+    this.bkname = bkname;
+  }
+  public String getBkno() {
+    return bkno;
+  }
+  public void setBkno(String bkno) {
+    this.bkno = bkno;
+  }
+  public Date getRgdt() {
+    return rgdt;
+  }
+  public void setRgdt(Date rgdt) {
+    this.rgdt = rgdt;
+  }
+  public char getStat() {
+    return stat;
+  }
+  public void setStat(char stat) {
+    this.stat = stat;
+  }
+  public String getNote() {
+    return note;
+  }
+  public void setNote(String note) {
+    this.note = note;
+  }
+  public char getMtstat() {
+    return mtstat;
+  }
+  public void setMtstat(char mtstat) {
+    this.mtstat = mtstat;
+  }
+  public Date getRgdtmt() {
+    return rgdtmt;
+  }
+  public void setRgdtmt(Date rgdtmt) {
+    this.rgdtmt = rgdtmt;
+  }
+  public Classes getClasses() {
+    return classes;
+  }
+  public void setClasses(Classes classes) {
+    this.classes = classes;
+  }
+  public ClassOrder getClassorder() {
+    return classorder;
+  }
+  public void setClassorder(ClassOrder classorder) {
+    this.classorder = classorder;
+  }
+  public Timetable getTimetable() {
+    return timetable;
+  }
+  public void setTimetable(Timetable timetable) {
+    this.timetable = timetable;
+  }
   @Override
   public String toString() {
     return "Mentee [no=" + no + ", name=" + name + ", email=" + email + ", pwd=" + pwd + ", nick="
@@ -37,174 +161,6 @@ public class Mentee {
         + ", rgdtmt=" + rgdtmt + ", classes=" + classes + ", classorder=" + classorder
         + ", timetable=" + timetable + "]";
   }
-  public Date getRgdtmt() {
-    return rgdtmt;
-  }
-  public void setRgdtmt(Date rgdtmt) {
-    this.rgdtmt = rgdtmt;
-  }
-
   
-
-  public Classes getClasses() {
-    return classes;
-  }
-
-  public void setClasses(Classes classes) {
-    this.classes = classes;
-  }
-
-  public ClassOrder getClassorder() {
-    return classorder;
-  }
-
-  public void setClassorder(ClassOrder classorder) {
-    this.classorder = classorder;
-  }
-
-  public Timetable getTimetable() {
-    return timetable;
-  }
-
-  public void setTimetable(Timetable timetable) {
-    this.timetable = timetable;
-  }
-  public int getNo() {
-    return no;
-  }
-
-  public void setNo(int no) {
-    this.no = no;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public String getPwd() {
-    return pwd;
-  }
-
-  public void setPwd(String pwd) {
-    this.pwd = pwd;
-  }
-
-  public String getNick() {
-    return nick;
-  }
-
-  public void setNick(String nick) {
-    this.nick = nick;
-  }
-
-  public String getPhone() {
-    return phone;
-  }
-
-  public void setPhone(String phone) {
-    this.phone = phone;
-  }
-
-  public String getPstno() {
-    return pstno;
-  }
-
-  public void setPstno(String pstno) {
-    this.pstno = pstno;
-  }
-
-  public String getBas_addr() {
-    return bas_addr;
-  }
-
-  public void setBas_addr(String bas_addr) {
-    this.bas_addr = bas_addr;
-  }
-
-  public String getDet_addr() {
-    return det_addr;
-  }
-
-  public void setDet_addr(String det_addr) {
-    this.det_addr = det_addr;
-  }
-
-  public String getPhot() {
-    return phot;
-  }
-
-  public void setPhot(String phot) {
-    this.phot = phot;
-  }
-
-  public char getSell() {
-    return sell;
-  }
-
-  public void setSell(char sell) {
-    this.sell = sell;
-  }
-
-  public String getBkname() {
-    return bkname;
-  }
-
-  public void setBkname(String bkname) {
-    this.bkname = bkname;
-  }
-
-  public int getBkno() {
-    return bkno;
-  }
-
-  public void setBkno(int bkno) {
-    this.bkno = bkno;
-  }
-
-  public Date getRgdt() {
-    return rgdt;
-  }
-
-  public void setRgdt(Date rgdt) {
-    this.rgdt = rgdt;
-  }
-
-  public char getStat() {
-    return stat;
-  }
-
-  public void setStat(char stat) {
-    this.stat = stat;
-  }
-
-  public String getNote() {
-    return note;
-  }
-
-  public void setNote(String note) {
-    this.note = note;
-  }
-
-  public char getMtstat() {
-    return mtstat;
-  }
-
-  public void setMtstat(char mtstat) {
-    this.mtstat = mtstat;
-  }
   
-
-
 }
--- src/main/webapp/WEB-INF/jsp/class/detail.jsp
+++ src/main/webapp/WEB-INF/jsp/class/detail.jsp
@@ -12,7 +12,7 @@
 <meta name="description" content="">
 <meta name="author" content="">
 
-<title>Clean Blog - Start Bootstrap Theme</title>
+<title>하루 - 클래스</title>
 
 <!-- Bootstrap core CSS -->
 <link href="/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
