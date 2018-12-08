38e1d2925d45d9d0ce665e43c8e0fa955a303449
Kei <kei@KEIui-MacBook-Pro.local>
2018-12-08 13:43:07 +0900

    문제 해결

--- src/main/java/bitcamp/java110/cms/service/CsService.java
+++ src/main/java/bitcamp/java110/cms/service/CsService.java
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-package bitcamp.java110.cms.service;
-
-import java.util.List;
-import bitcamp.java110.cms.domain.Cs;
-
-public interface CsService {
-  int add(Cs cs);
-  List<Cs> list(int pageNo, int pageSize,int meno);
-  Cs get(int no);
-  void delete(int no);
-  int update(Cs cs);
-  List<Cs>findByMaster();
-  List<Cs> pageMaster(int pageNo, int pageSize);
-  
-  
-=======
 package bitcamp.java110.cms.service;
 
 import java.util.List;
@@ -28,7 +11,6 @@ public interface CsService {
   int update(Cs cs);
   List<Cs>findByMaster();
   int addQuestioninfo(Cs cs);
+  List<Cs> pageMaster(int pageNo, int pageSize);
   
-  
->>>>>>> c9f11a73504a471e64258e5ad17458bfc3fdfab8
 }
